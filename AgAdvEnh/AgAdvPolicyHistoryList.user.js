ecc693cec259c44c24b7ef11efc2b5ebaa320dbe
Christopher Reeber <creeber@agrilogic.com>
2014-12-24 13:43:47 -0500

    Meta information

--- AgAdvEnh/AgAdvPolicyHistoryList.user.js
+++ AgAdvEnh/AgAdvPolicyHistoryList.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name         AgAdv Policy History List 
+// @name         Ag Adv Policy History List 
 // @namespace    http://www.makemea.ninja 
-// @version      0.3
+// @version      1.0
 // @author       Christopher Reeber 
 // @match        http*://localhost/AgAdvantage*
 // @match        http*://localhost/AgriLogic.Web*
 // @grant        unsafeWindow
 // @grant        GM_addStyle
-// @grant    	 GM_getResourceText
+// @grant    	GM_getResourceText
 // @require      http://code.jquery.com/jquery-latest.js
 // @resource  	pnlhtml		https://raw.githubusercontent.com/dragonalighted/Tamper/master/AgAdvEnh/panel.html
 // @resource  	itmhtml  	https://raw.githubusercontent.com/dragonalighted/Tamper/master/AgAdvEnh/history_item.html
 // @resource  	pnlcss		https://raw.githubusercontent.com/dragonalighted/Tamper/master/AgAdvEnh/panel.css
-// @downloadURL https://github.com/dragonalighted/Tamper/raw/master/AgAdvEnh/Policy%20History%20List%20for%20Search.user.js
-// @updateURL   https://github.com/dragonalighted/Tamper/raw/master/AgAdvEnh/Policy%20History%20List%20for%20Search.user.js
+// @downloadURL https://github.com/dragonalighted/Tamper/raw/master/AgAdvEnh/AgAdvPolicyHistoryList.user.js
+// @updateURL   https://github.com/dragonalighted/Tamper/raw/master/AgAdvEnh/AgAdvPolicyHistoryList.meta.js
 // ==/UserScript==
 
 
